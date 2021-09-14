182cf49513de95eee5688ecce729003c603f0833
41973452 <41973452+Felanbird@users.noreply.github.com>
2021-09-14 08:36:23 -0400

    Make the startup debug prompt grammatically correct with not great code

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,15 @@
 module.exports = async (client) => {
-    console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
+    if (client.guilds.cache.size && client.users.cache.size === 1)
+        console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} server, for a total of ${client.users.cache.size} user`);
+
+    else if (client.guilds.cache.size = 1 && client.users.cache.size > 1)
+        console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} server, for a total of ${client.users.cache.size} users`);
+
+    else if (client.guilds.cache.size > 1 && client.users.cache.size === 1)
+        console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} user`);
+
+    else
+        console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users`);
 
     client.user.setActivity(client.config.discord.activity);
 };
