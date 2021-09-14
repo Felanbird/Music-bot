11908db3e39b4a144d489fd4fa10ff9c34d913e6
41973452 <41973452+Felanbird@users.noreply.github.com>
2021-09-14 08:40:46 -0400

    Make the actual code correct at least

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = async (client) => {
     if (client.guilds.cache.size && client.users.cache.size === 1)
         console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} server, for a total of ${client.users.cache.size} user`);
 
-    else if (client.guilds.cache.size = 1 && client.users.cache.size > 1)
+    else if (client.guilds.cache.size === 1 && client.users.cache.size > 1)
         console.log(`Logged in as ${client.user.username}. Ready on ${client.guilds.cache.size} server, for a total of ${client.users.cache.size} users`);
 
     else if (client.guilds.cache.size > 1 && client.users.cache.size === 1)
