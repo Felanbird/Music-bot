f64024d0ce164c23f155aac4ea50901c3d772858
41973452 <41973452+Felanbird@users.noreply.github.com>
2021-09-14 08:04:08 -0400

    Make resume work

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -14,6 +14,9 @@ module.exports = {
         if (!client.player.getQueue(message).paused) return message.channel.send(`${client.emotes.error} - The music is already playing!`);
 
         const success = client.player.resume(message);
+        client.player.resume(message)
+        client.player.pause(message)
+        client.player.resume(message)
 
         if (success) message.channel.send(`${client.emotes.success} - Song ${client.player.getQueue(message).playing.title} resumed!`);
     },
